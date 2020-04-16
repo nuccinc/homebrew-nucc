2209fa15a65a5fc5f48a48c493a28e62c079dcdb
phx <oldjamey@gmail.com>
2020-04-16 13:03:28 -0500

    updated

--- nucc.rb
+++ nucc.rb
@@ -1,8 +1,8 @@
 class Nucc < Formula
   desc "COVID-19 research via distributed computing in collaboration with The National Upcycled Computing Collective."
   homepage "https://github.com/phx/nucc"
-  url "https://github.com/phx/homebrew-nucc/archive/0.0.11.tar.gz"
-  sha256 "29664b6aad9b1ec15f25f6f8a1039f0f035085b81766d958d19c1806d9c98413"
+  url "https://github.com/phx/homebrew-nucc/archive/0.0.13.tar.gz"
+  sha256 "af271a738e19198affa075ad51f1905b8edc1d5ddd443aa3ddab8e30b29f3f48"
 
   depends_on :boinc => "7.16.6"
 
