91057e966bdd092a507b2beaf53c6ea2a35efacb
phx <oldjamey@gmail.com>
2020-04-16 13:02:10 -0500

    updated

--- nucc.rb
+++ nucc.rb
@@ -4,8 +4,7 @@ class Nucc < Formula
   url "https://github.com/phx/homebrew-nucc/archive/0.0.11.tar.gz"
   sha256 "29664b6aad9b1ec15f25f6f8a1039f0f035085b81766d958d19c1806d9c98413"
 
-  depends_on "homebrew/cask"
-  depends_on "homebrew/cask-boinc" => "7.16.6"
+  depends_on :boinc => "7.16.6"
 
   def install
     # move 'quickstart.sh' to #{prefix}/bin/nucc-install
