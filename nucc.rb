423d13f92fc0e953a62c8904e8519b9dde608066
phx <oldjamey@gmail.com>
2020-04-16 13:22:15 -0500

    updated

--- nucc.rb
+++ nucc.rb
@@ -1,18 +1,15 @@
-class Nucc < Formula
-  desc "COVID-19 research via distributed computing in collaboration with The National Upcycled Computing Collective."
+cask 'nucc' do
+  version '2.7.1_387'
+  sha256 'a3738d0513d736918a6d71535ef3d85dd184af267c05698e49ac4c6b48f38e17'
+
+  url ""
+  name 'nucc'
   homepage "https://github.com/phx/nucc"
-  url "https://github.com/phx/homebrew-nucc/archive/0.0.13.tar.gz"
-  sha256 "af271a738e19198affa075ad51f1905b8edc1d5ddd443aa3ddab8e30b29f3f48"
 
   depends_on :boinc => "7.16.6"
 
-  def install
-    # move 'quickstart.sh' to #{prefix}/bin/nucc-install
-    # bin.install "quickstart.sh" => "nucc-install"
-    system "./quickstart.sh"
-  end
-
-  test do
-    system "false"
+  postflight do
+    system_command './quickstart.sh',
+                   sudo: false
   end
 end
