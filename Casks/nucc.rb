4e28a193f1c1c2c8e3504a3812ebb618c5f48793
phx <oldjamey@gmail.com>
2020-04-16 13:37:37 -0500

    updated

--- Casks/nucc.rb
+++ Casks/nucc.rb
@@ -1,8 +1,8 @@
 cask 'nucc' do
-  version '0.0.2'
-  sha256 "24d3a2b299ebfc4820959d35af0f45476633bcb37b2f6df819c094465cef62ca"
+  version '0.0.3'
+  sha256 "5dfbc56730cf2e6d44a820e0caa60a8c0593ca61602d10d0ba32bbd773c7b5b5"
 
-  url "https://github.com/phx/homebrew-nucc/archive/0.0.2.tar.gz"
+  url "https://github.com/phx/homebrew-nucc/archive/0.0.3.tar.gz"
   name 'nucc'
   homepage "https://github.com/phx/nucc"
 
