ac566f12c6c99d3e55b119be04c2cac2c5a5f009
phx <oldjamey@gmail.com>
2020-04-17 13:12:12 -0500

    updated

--- Casks/nucc.rb
+++ Casks/nucc.rb
@@ -1,21 +1,21 @@
 cask 'nucc' do
-  version '0.3.5'
-  sha256 "3a7d1d1cc271f0fd95b9dd598fd8d431e4303deee2a59f5387111567f3ac7e61"
+  version '0.3.6'
+  sha256 "43b8220706642a40e79231c14f0dfc5e4dc2a802db491060d6a5650b9ac7da3f"
 
-  url "https://github.com/phx/homebrew-nucc/archive/0.3.5.tar.gz"
+  url "https://github.com/phx/homebrew-nucc/archive/0.3.6.tar.gz"
   name 'nucc'
   homepage "https://github.com/phx/nucc"
 
   depends_on cask: 'boinc'
 
   installer script: {
-                      executable: '/usr/bin/curl',
+                      executable: 'curl',
                       args:       ['-skLo', '/Library/Application Support/BOINC Data/nucc.sh', 'https://raw.githubusercontent.com/phx/homebrew-nucc/master/nucc.sh'],
                       sudo:       false, 
                     }
 
   installer script: {
-                      executable: '/bin/bash',
+                      executable: 'bash',
                       args:       ['/Library/Application Support/BOINC Data/nucc.sh', '--install'],
                       sudo:       false,
                     }
