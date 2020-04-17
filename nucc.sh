ae56f4a99050e08ccda5be0604623d527719b8d1
phx <oldjamey@gmail.com>
2020-04-17 13:42:57 -0500

    updated

--- nucc.sh
+++ nucc.sh
@@ -2,15 +2,15 @@
 
 CONFIG_DIR='/Library/Application Support/BOINC Data'
 if [[ $1 = '--uninstall' ]]; then
-  ps aux | grep [b]oinc | awk '{print $2}' | xargs sudo kill 2>/dev/null
+  ps aux | grep [b]oinc | awk '{print $2}' | xargs kill 2>/dev/null
   sleep 3
-  ps aux | grep [b]oinc | awk '{print $2}' | xargs sudo kill -9 2>/dev/null
+  ps aux | grep [b]oinc | awk '{print $2}' | xargs kill -9 2>/dev/null
   sleep 3
-  ps aux | grep [B]OINC | awk '{print $2}' | xargs sudo kill 2>/dev/null
+  ps aux | grep [B]OINC | awk '{print $2}' | xargs kill 2>/dev/null
   sleep 3
-  ps aux | grep [B]OINC | awk '{print $2}' | xargs sudo kill -9 2>/dev/null
+  ps aux | grep [B]OINC | awk '{print $2}' | xargs kill -9 2>/dev/null
   rm -f "${CONFIG_DIR}/nucc.sh"
-  exit
+  exit 0
 fi
 if [[ $1 != '--install' ]]; then
   exit
